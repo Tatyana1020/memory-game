acaf2fa4222f8bb08e7749de5b11d93ff0043b07
Tatyana <tatyanatyazhkih@gmail.com>
2026-06-07 23:21:27 +0300

    update cors

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    window.location.hostname === "[::1]" ||
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+
+const API_URL = isLocalhost 
+  ? "http://localhost:5000/" 
+  : "https://k5fwtc9b-5000.euw.devtunnels.ms/";
+
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: API_URL
 });
 
 const $authHost = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: API_URL
 });
 
 const authInterceptor = config => {
